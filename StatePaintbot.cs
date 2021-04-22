ce5c777a935b9c5a76c8fc7f77ae4abc46f47ec6
Mårten Åsberg <marten.asberg@outlook.com>
2021-04-22 11:25:28 +0200

    Cache information within ticks

--- StatePaintbot.cs
+++ StatePaintbot.cs
@@ -16,6 +16,68 @@ namespace PaintBot
 		protected string PlayerId { get; private set; }
 		protected MapUtils MapUtils { get; private set; }
 
+		private long? totalGameTicks = null;
+		protected long TotalGameTicks
+		{
+			get
+			{
+				if (totalGameTicks.HasValue)
+				{
+					return totalGameTicks.Value;
+				}
+				else
+				{
+					totalGameTicks = GameSettings.GameDurationInSeconds * 1000 / GameSettings.TimeInMsPerTick;
+					return totalGameTicks.Value;
+				}
+			}
+		}
+
+		private long playerInfoCache = -1;
+		private CharacterInfo playerInfo = null;
+		protected CharacterInfo PlayerInfo
+		{
+			get
+			{
+				if (playerInfoCache != Map.WorldTick)
+				{
+					playerInfo = MapUtils.GetCharacterInfoFor(PlayerId);
+					playerInfoCache = Map.WorldTick;
+				}
+				return playerInfo;
+			}
+		}
+
+		private long playerCoordinateCache = -1;
+		private MapCoordinate playerCoordinate = null;
+		protected MapCoordinate PlayerCoordinate
+		{
+			get
+			{
+				if (playerCoordinateCache != Map.WorldTick)
+				{
+					playerCoordinate = MapUtils.GetCoordinateFrom(PlayerInfo.Position);
+					playerCoordinateCache = Map.WorldTick;
+				}
+				return playerCoordinate;
+			}
+		}
+
+		private long playerColouredCoordinatesCache = -1;
+		private MapCoordinate[] playerColouredCoordinates = null;
+		protected MapCoordinate[] PlayerColouredCoordinates
+		{
+			get
+			{
+				if (playerColouredCoordinatesCache != Map.WorldTick)
+				{
+					playerColouredCoordinates = MapUtils.GetCoordinatesFrom(PlayerInfo.ColouredPositions);
+					playerColouredCoordinatesCache = Map.WorldTick;
+				}
+				return playerColouredCoordinates;
+			}
+		}
+
 		private IEnumerator<Action> currentActionSequence;
 
 		protected StatePaintBot(PaintBotConfig paintBotConfig, IPaintBotClient paintBotClient, IHearBeatSender heartBeatSender, ILogger logger) :
